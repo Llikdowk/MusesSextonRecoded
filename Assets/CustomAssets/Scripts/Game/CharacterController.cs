86ce8f8f1c4210f239d5ba5333d033198443d5e0
Llikdowk <guillermofm7@gmail.com>
2017-02-03 11:55:53 +0100

    OverlapCapsule stops colliding with Player

--- Assets/CustomAssets/Scripts/Game/CharacterController.cs
+++ Assets/CustomAssets/Scripts/Game/CharacterController.cs
@@ -7,18 +7,19 @@ public class CharacterController : MonoBehaviour {
 	private CapsuleCollider _collider;
 	private Camera _camera;
 	private Transform _transform;
+	private int _layerMaskAllButPlayer;
 
 	private GameObject _debugSphere1;
 	private GameObject _debugSphere2;
 
-	public void Awake() {
+
+	public void Start() {
 		_debugSphere1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 		_debugSphere2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 		Object.Destroy(_debugSphere1.GetComponent<Collider>());
 		Object.Destroy(_debugSphere2.GetComponent<Collider>());
-	}
-
-	public void Start() {
+		int player = 1 << LayerMaskManager.GetInstance().GetLayer(Layer.Player);
+		_layerMaskAllButPlayer = ~player;
 		_camera = GetComponentInChildren<Camera>();
 		_collider = GetComponent<CapsuleCollider>();
 		_transform = GetComponent<Transform>();
@@ -32,7 +33,7 @@ public class CharacterController : MonoBehaviour {
 		Matrix4x4 M = _collider.transform.localToWorldMatrix;
 		Vector3 p1 = _transform.position + M.MultiplyVector(_collider.center) + transform.up * (_collider.height/2.0f - _collider.radius);
 		Vector3 p2 = _transform.position + M.MultiplyVector(_collider.center) - transform.up * (_collider.height/2.0f - _collider.radius);
-		Collider[] colliders = Physics.OverlapCapsule(p1, p2, _collider.radius);
+		Collider[] colliders = Physics.OverlapCapsule(p1, p2, _collider.radius, _layerMaskAllButPlayer);
 
 		_debugSphere1.transform.position = p1;
 		_debugSphere2.transform.position = p2;
---
+++ Assets/CustomAssets/Scripts/LayerMaskManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Layer {
+	Player
+}
+
+public class LayerMaskManager {
+	private static LayerMaskManager _instance;
+	private readonly Dictionary<string, int> _map;
+
+	public static LayerMaskManager GetInstance() {
+		return _instance ?? (_instance = new LayerMaskManager());
+	}
+
+	private LayerMaskManager() {
+		_map = new Dictionary<string, int>();
+		foreach (Layer item in Enum.GetValues(typeof(Layer))) {
+			AddLayer(item);
+		}
+	}
+
+	public void AddLayer(Layer layer) {
+		string name = layer.ToString();
+		_map.Add(name, LayerMask.NameToLayer(name));
+	}
+
+	public int GetLayer(Layer layer) {
+		return _map[layer.ToString()];
+	}
+
+}
