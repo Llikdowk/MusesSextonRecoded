37cfbd35c07c40ee37da1c9aab39e34f23237646
Llikdowk <guillermofm7@gmail.com>
2017-02-04 21:57:28 +0100

    add a simple and rough fade out for the intro image

--- Assets/CustomAssets/Scripts/LoadComponents.cs
+++ Assets/CustomAssets/Scripts/LoadComponents.cs
@@ -13,8 +13,11 @@ public class LoadComponents : MonoBehaviour {
 	private float _originalTimeScale;
 	private readonly ActionManager _actions = ActionManager.GetInstance();
 	private readonly List<GameObject> _startEnabledComponents = new List<GameObject>();
+	private bool _hasClicked = false;
 
 	public void Awake() {
+		Terrain.gameObject.SetActive(true);
+		Terrain.enabled = true;
 		_originalTimeScale = Time.timeScale;
 		Time.timeScale = 0.0f;
 		_actions.AllowActions = false;
@@ -23,28 +26,23 @@ public class LoadComponents : MonoBehaviour {
 	public void Start() {
 		TitleImage.gameObject.SetActive(true);
 		TitleImage.enabled = true;
-		if (FallbackImage) {
-			FallbackImage.gameObject.SetActive(true);
-			FallbackImage.enabled = true;
-		}
+		FallbackImage.gameObject.SetActive(true);
+		FallbackImage.enabled = true;
 	}
 
 	public void Update() {
-		if (Terrain.isMeshSyncronized) {
+		if (Terrain.isMeshSyncronized && _hasClicked) {
 			Time.timeScale = _originalTimeScale;
 			enabled = false;
 			TitleImage.gameObject.SetActive(false);
-			if (FallbackImage) {
-				FallbackImage.gameObject.SetActive(false);
-			}
+			FallbackImage.gameObject.SetActive(false);
 			_actions.AllowActions = true;
 		}
 		else {
 			if (Input.GetKeyDown(KeyCode.Mouse0)) {
-				TitleImage.gameObject.SetActive(false);
-				if (FallbackImage) {
-					FallbackImage.gameObject.SetActive(true);
-				}
+				_hasClicked = true;
+				StartCoroutine(Utils.FadeOut(TitleImage, 1.0f));
+				FallbackImage.gameObject.SetActive(true);
 			}
 		}
 	}
---
+++ Assets/CustomAssets/Scripts/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class Utils {
+	public static IEnumerator FadeOut(Image img, float speed) {
+		const float dt = 0.033f;
+		float t = 0.0f;
+		Color original = img.color;
+		Color final = img.color;
+		final.a = 0.0f;
+		while (t < 1.0f) {
+			img.color = Color.Lerp(original, final, t);
+			t += dt * speed;
+			if (Time.timeScale == 0.0f) { // TODO: find a workaround for this hack
+				yield return null; //new WaitForSeconds(dt);
+			}
+			else {
+				yield return new WaitForEndOfFrame();
+			}
+		}
+	}
+
+}
