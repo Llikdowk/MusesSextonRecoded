fccc7f6a871a305f7180ae28c8ef34fc1b3cfabf
Llikdowk <guillermofm7@gmail.com>
2017-02-05 21:06:06 +0100

    merge lookVertical and lookHorizontal into look single component

---
+++ Assets/CustomAssets/Scripts/Game/CustomInput/Look.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+namespace Game.CustomInput {
+	public class Look : MonoBehaviour {
+
+		[Range(0.0f, 200.0f)] public float XSensitivity = 100.0f;
+		[Range(0.0f, 200.0f)] public float YSensitivity = 100.0f;
+		[Range(0.0f, 1.0f)] public float VerticalRange = 0.9f;
+		public bool Smooth = false;
+		private Camera _camera;
+
+		public void Awake() {
+			_camera = GetComponentInChildren<Camera>();
+		}
+
+		public void Update() {
+			const string horizontalAxisName = "Mouse X";
+			float axisValue = Smooth ? Input.GetAxis(horizontalAxisName) : Input.GetAxisRaw(horizontalAxisName);
+			float speed = axisValue * Time.deltaTime * XSensitivity;
+			transform.Rotate(0, speed, 0, Space.Self);
+
+
+			const string verticalAxisName = "Mouse Y";
+			axisValue = Smooth ? Input.GetAxis(verticalAxisName) : Input.GetAxisRaw(verticalAxisName);
+			speed = axisValue * Time.deltaTime * YSensitivity;
+
+			_camera.transform.Rotate(-speed, 0, 0, Space.Self);
+			float d = Vector3.Dot(_camera.transform.forward, transform.forward);
+			if (d < 1 - VerticalRange) {
+				_camera.transform.Rotate(speed, 0, 0, Space.Self);
+			}
+		}
+
+	}
+}
