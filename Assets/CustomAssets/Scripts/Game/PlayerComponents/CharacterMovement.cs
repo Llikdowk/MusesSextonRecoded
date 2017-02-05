f8398130d702e9a63729defe53e18658efe4b7f8
Llikdowk <guillermofm7@gmail.com>
2017-02-05 14:30:45 +0100

    add smooth input

--- Assets/CustomAssets/Scripts/Game/PlayerComponents/CharacterMovement.cs
+++ Assets/CustomAssets/Scripts/Game/PlayerComponents/CharacterMovement.cs
@@ -1,4 +1,5 @@
-﻿using Game.CustomInput;
+﻿using System.ComponentModel;
+using Game.CustomInput;
 using UnityEngine;
 
 namespace Game.PlayerComponents {
@@ -8,78 +9,106 @@ namespace Game.PlayerComponents {
 	public class CharacterMovement : MonoBehaviour {
 
 		private class InputMovement {
-			public Vector3 SelfDir { get; private set; }
+			public Vector3 SelfMovement { get { return _selfMovement; } }
+			private Vector3 _selfMovement;
 
-			public void SetRawMovement() {
+			private readonly float _speedUp, _speedDown;
+
+			public InputMovement(float speedUp, float speedDown) {
+				_speedUp = speedUp;
+				_speedDown = speedDown;
+			}
+
+			public void SetRawMovement() { // TODO extract (this does not need SpeedUp/Down params!
 				ActionManager actions = Player.GetInstance().Actions;
 				
 				Action actionForward = actions.GetAction(ActionTag.MoveForward);
 				actionForward.StartBehaviour = () => {
-					SelfDir += Vector3.forward;
+					_selfMovement.z += 1.0f; //Vector3.forward;
 				};
 				actionForward.FinishBehaviour = () => {
-					SelfDir -= Vector3.forward;
+					_selfMovement.z -= 1.0f; //Vector3.forward;
 				};
 				
 				Action actionBack = actions.GetAction(ActionTag.MoveBack);
 				actionBack.StartBehaviour = () => {
-					SelfDir += Vector3.back;
+					_selfMovement.z += -1.0f; //Vector3.back;
 				};
 				actionBack.FinishBehaviour = () => {
-					SelfDir -= Vector3.back;
+					_selfMovement.z -= -1.0f; //Vector3.back;
 				};
 
 				Action actionLeft = actions.GetAction(ActionTag.MoveLeft);
 				actionLeft.StartBehaviour = () => {
-					SelfDir += Vector3.left;
+					_selfMovement.x += -1.0f; //Vector3.left;
 				};
 				actionLeft.FinishBehaviour = () => {
-					SelfDir -= Vector3.left;
+					_selfMovement.x -= -1.0f; //Vector3.left;
 				};
 
 				Action actionRight = actions.GetAction(ActionTag.MoveRight);
 				actionRight.StartBehaviour = () => {
-					SelfDir += Vector3.right;
+					_selfMovement.x += 1.0f;  //Vector3.right;
 				};
 				actionRight.FinishBehaviour = () => {
-					SelfDir -= Vector3.right;
+					_selfMovement.x -= 1.0f; //Vector3.right;
 				};
 			}
 
 
-			public void SetSmoothMovement() { // TODO: change FinishBehaviour by NotPressedBehaviour
+			public void SetSmoothMovement() { // TODO extract
 				ActionManager actions = Player.GetInstance().Actions;
 				
 				Action actionForward = actions.GetAction(ActionTag.MoveForward);
-				actionForward.StartBehaviour = () => {
-					SelfDir += Vector3.forward;
+				actionForward.WhileBehaviour = () => {
+					_selfMovement.z = Mathf.Min(_selfMovement.z + _speedUp * Time.deltaTime, 1.0f);
 				};
 				actionForward.FinishBehaviour = () => {
-					SelfDir -= Vector3.forward;
+					actionForward.NotPressedBehaviour = () => {
+						_selfMovement.z = Mathf.Max(_selfMovement.z - _speedDown * Time.deltaTime, 0.0f);
+						if (_selfMovement.z == 0.0f) {
+							actionForward.NotPressedBehaviour = Action.nop;
+						}
+					};
 				};
-				
+
 				Action actionBack = actions.GetAction(ActionTag.MoveBack);
-				actionBack.StartBehaviour = () => {
-					SelfDir += Vector3.back;
+				actionBack.WhileBehaviour = () => {
+					_selfMovement.z = Mathf.Max(_selfMovement.z - _speedUp * Time.deltaTime, -1.0f);
 				};
 				actionBack.FinishBehaviour = () => {
-					SelfDir -= Vector3.back;
+					actionBack.NotPressedBehaviour = () => {
+						_selfMovement.z = Mathf.Min(_selfMovement.z + _speedDown * Time.deltaTime, 0.0f);
+						if (_selfMovement.z == 0.0f) {
+							actionBack.NotPressedBehaviour = Action.nop;
+						}
+					};
 				};
 
 				Action actionLeft = actions.GetAction(ActionTag.MoveLeft);
-				actionLeft.StartBehaviour = () => {
-					SelfDir += Vector3.left;
+				actionLeft.WhileBehaviour = () => {
+					_selfMovement.x = Mathf.Max(_selfMovement.x - _speedUp * Time.deltaTime, -1.0f);
 				};
 				actionLeft.FinishBehaviour = () => {
-					SelfDir -= Vector3.left;
+					actionLeft.NotPressedBehaviour = () => {
+						_selfMovement.x = Mathf.Min(_selfMovement.x + _speedDown * Time.deltaTime, 0.0f);
+						if (_selfMovement.x == 0.0f) {
+							actionLeft.NotPressedBehaviour = Action.nop;
+						}
+					};
 				};
 
 				Action actionRight = actions.GetAction(ActionTag.MoveRight);
-				actionRight.StartBehaviour = () => {
-					SelfDir += Vector3.right;
+				actionRight.WhileBehaviour = () => {
+					_selfMovement.x = Mathf.Min(_selfMovement.x + _speedUp * Time.deltaTime, 1.0f);
 				};
 				actionRight.FinishBehaviour = () => {
-					SelfDir -= Vector3.right;
+					actionRight.NotPressedBehaviour = () => {
+						_selfMovement.x = Mathf.Max(_selfMovement.x - _speedDown * Time.deltaTime, 0.0f);
+						if (_selfMovement.x == 0.0f) {
+							actionRight.NotPressedBehaviour = Action.nop;
+						}
+					};
 				};
 			}
 
@@ -91,8 +120,11 @@ namespace Game.PlayerComponents {
 		public float BackwardSpeed = 5.0f;
 		public float LeftSpeed = 5.0f;
 		public float RightSpeed = 5.0f;
-		public readonly float SpeedUp = 6.0f;
-		public Vector3 SelfDir { get { return _input.SelfDir.normalized; } }
+		public float SpeedUp = 1.0f;
+		public float SpeedDown = 1.0f;
+		public Vector3 SelfMovement { get { return _input.SelfMovement; } }
+		public Vector3 WorldMovement { get { return transform.localToWorldMatrix.MultiplyVector(_input.SelfMovement); } }
+		public Vector3 SelfDir { get { return _input.SelfMovement.normalized; } }
 		public Vector3 WorldDir { get { return transform.localToWorldMatrix.MultiplyVector(SelfDir); } }
 
 		public Vector3 StepMovement {
@@ -101,8 +133,8 @@ namespace Game.PlayerComponents {
 		private Vector3 _stepMovement;
 
 		public void Start() { 
-			_input = new InputMovement();
-			_input.SetRawMovement();
+			_input = new InputMovement(SpeedUp, SpeedDown);
+			_input.SetSmoothMovement();
 		}
 
 		public void AddForce(Vector3 dir, float force) {
@@ -114,10 +146,14 @@ namespace Game.PlayerComponents {
 		public void Update() {
 			transform.position += _stepMovement;
 			_stepMovement = Vector3.zero;
-			//Vector3 speed = Vector3.zero;
-			//speed.x = _selfDir.x * _selfDir.x > 0 ? RightSpeed : LeftSpeed;
-			//speed.z = _selfDir.z * _selfDir.z > 0 ? ForwardSpeed : BackwardSpeed;
-			_stepMovement += WorldDir * ForwardSpeed * Time.deltaTime;
+			Vector3 v;
+			if (WorldMovement.sqrMagnitude < 1.0f) {
+				v = WorldMovement;
+			}
+			else {
+				v = WorldDir;
+			}
+			_stepMovement += v * ForwardSpeed * Time.deltaTime;
 
 		}
 
