1eeaad4acfebc5e78e632dca69bbdd483fe08f37
Llikdowk <guillermofm7@gmail.com>
2017-02-06 10:23:33 +0100

    add custom input configuration in the Inspector

--- Assets/CustomAssets/Scripts/Game/CustomInput/InputController.cs
+++ Assets/CustomAssets/Scripts/Game/CustomInput/InputController.cs
@@ -1,25 +1,53 @@
-﻿using Game.PlayerComponents;
+﻿using System;
+using Game.PlayerComponents;
 using UnityEngine;
 
 namespace Game.CustomInput {
 
 	public class InputController : MonoBehaviour {
 
+		[Serializable]
+		public struct InputAction {
+			 public PlayerAction Action;
+			 public KeyCode[] Keys;
+		 }
+
+		public InputAction[] InputConfig;
+
 		private ActionManager<PlayerAction> _actions;
+
 
 		public void Start() {
 			_actions = Player.GetInstance().Actions;
 
-			_actions.GetAction(PlayerAction.MoveForward).AddKey(KeyCode.W);
-			_actions.GetAction(PlayerAction.MoveLeft).AddKey(KeyCode.A);
-			_actions.GetAction(PlayerAction.MoveBack).AddKey(KeyCode.S);
-			_actions.GetAction(PlayerAction.MoveRight).AddKey(KeyCode.D);
-			_actions.GetAction(PlayerAction.Use).AddKey(KeyCode.Mouse0).AddKey(KeyCode.E);
-			_actions.GetAction(PlayerAction.Run).AddKey(KeyCode.LeftShift);
+			if (InputConfig.Length == 0) {
+				InputConfig = new InputAction[Enum.GetValues(typeof(PlayerAction)).Length];
+
+				InputConfig[0].Action = PlayerAction.MoveForward;
+				InputConfig[0].Keys = new[] {KeyCode.W};
+				InputConfig[1].Action = PlayerAction.MoveLeft;
+				InputConfig[1].Keys = new[] {KeyCode.A};
+				InputConfig[2].Action = PlayerAction.MoveBack;
+				InputConfig[2].Keys = new[] {KeyCode.S};
+				InputConfig[3].Action = PlayerAction.MoveRight;
+				InputConfig[3].Keys = new[] {KeyCode.D};
+
+				InputConfig[4].Action = PlayerAction.Use;
+				InputConfig[4].Keys = new[] {KeyCode.E, KeyCode.Mouse0};
+				InputConfig[5].Action = PlayerAction.Run;
+				InputConfig[5].Keys = new[] {KeyCode.LeftShift};
+			}
+
+			foreach (InputAction inputAction in InputConfig) {
+				var action = _actions.GetAction(inputAction.Action);
+				foreach (KeyCode k in inputAction.Keys) {
+					action.AddKey(k);
+				}
+			}
 
 		}
 
-		public void Update() { // Executed before anything else (check ScriptOrder)
+		public void Update() { // Executed before any other script Update (check ScriptOrder)
 
 			foreach (Action<PlayerAction> action in _actions.Actions) {
 				foreach (KeyCode k in action.Keys) {
