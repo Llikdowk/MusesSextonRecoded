decb5396f78c2a9cd1613deaddc7ff5a4bb2acb3
Llikdowk <guillermofm7@gmail.com>
2017-02-07 11:26:41 +0100

    Add look smooth functionality

--- Assets/CustomAssets/Scripts/Game/PlayerComponents/Look.cs
+++ Assets/CustomAssets/Scripts/Game/PlayerComponents/Look.cs
@@ -4,35 +4,68 @@ using UnityEngine;
 namespace Game.PlayerComponents {
 	public class Look : MonoBehaviour {
 
+		public bool SmoothInput = false;
 		[Range(0, 1)] public float XSensitivity = 0.5f;
 		[Range(0, 1)] public float YSensitivity = 0.5f;
-		[Range(0, 90)] public float VerticalRangeDegrees = 90.0f;
+		public float SensitivityMultiplicator = 200.0f;
 
-		public bool Smooth = false;
+		public bool SmoothVerticalClipping = false;
+		[Range(0, 90)] public float VerticalRangeDegrees = 90.0f;
+		[Range(0, 1)] public float VerticalForceBack = 0.5f;
+
+		public bool SmoothHorizontalClipping = false;
+		[Range(0, 181)] public float HorizontalRangeDegrees = 181.0f;
+		[Range(0, 1)] public float HorizontalForceBack = 0.5f;
+		public Transform FixedForward;
+
 		private Camera _camera;
+
 
 		public void Awake() {
 			_camera = GetComponentInChildren<Camera>();
 		}
 
+		public void Start() {
+			if (FixedForward == null) {
+				Debug.LogWarning("Not Fixed Forward found: initialized with this object transform!");
+				FixedForward = transform;
+			}
+		}
+
 		public void Update() {
-			float xSensitivity = XSensitivity * 200.0f;
-			float ySensitivity = YSensitivity * 200.0f;
+			float xSensitivity = XSensitivity * SensitivityMultiplicator;
+			float ySensitivity = YSensitivity * SensitivityMultiplicator;
 
 			const string horizontalAxisName = "Mouse X";
-			float axisValue = Smooth ? Input.GetAxis(horizontalAxisName) : Input.GetAxisRaw(horizontalAxisName);
+			float axisValue = SmoothInput ? Input.GetAxis(horizontalAxisName) : Input.GetAxisRaw(horizontalAxisName);
 			float speed = axisValue * Time.deltaTime * xSensitivity;
-			transform.Rotate(0, speed, 0, Space.Self);
-			//TODO horizontal clip
+
+			Quaternion rotation = Quaternion.AngleAxis(speed, Vector3.up) * transform.rotation;
+			float angle = Quaternion.Angle(rotation, FixedForward.rotation);
+			if (SmoothHorizontalClipping) {
+				transform.rotation = Quaternion.Slerp(rotation, transform.rotation,
+					HorizontalForceBack * angle / HorizontalRangeDegrees);
+			}
+			else {
+				if (angle < HorizontalRangeDegrees) {
+					transform.rotation = rotation;
+				}
+			}
 
 			const string verticalAxisName = "Mouse Y";
-			axisValue = Smooth ? Input.GetAxis(verticalAxisName) : Input.GetAxisRaw(verticalAxisName);
+			axisValue = SmoothInput ? Input.GetAxis(verticalAxisName) : Input.GetAxisRaw(verticalAxisName);
 			speed = axisValue * Time.deltaTime * ySensitivity;
-
-			Quaternion rotation = Quaternion.AngleAxis(speed, Vector3.left) * _camera.transform.localRotation;
-			float angle = rotation.eulerAngles.x;
-			if ((angle < VerticalRangeDegrees || angle > 360.0f - VerticalRangeDegrees) && rotation.eulerAngles.y < 179.0f) {
-				_camera.transform.localRotation = rotation;
+			rotation = Quaternion.AngleAxis(speed, Vector3.left) * _camera.transform.localRotation;
+			angle = Quaternion.Angle(rotation, Quaternion.AngleAxis(0, transform.forward));
+			Debug.Log(angle);
+			if (SmoothVerticalClipping) {
+				_camera.transform.localRotation = Quaternion.Slerp(rotation, _camera.transform.localRotation,
+					VerticalForceBack * angle / VerticalRangeDegrees);
+			}
+			else {
+				if (angle < VerticalRangeDegrees) {
+					_camera.transform.localRotation = rotation;
+				}
 			}
 		}
 	}
