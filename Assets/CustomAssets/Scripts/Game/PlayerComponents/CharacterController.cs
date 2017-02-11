3ece25886056f76fb83b9e71176c79d269476fb1
Llikdowk <guillermofm7@gmail.com>
2017-02-11 14:49:17 +0100

    improve character controller

--- Assets/CustomAssets/Scripts/Game/PlayerComponents/CharacterController.cs
+++ Assets/CustomAssets/Scripts/Game/PlayerComponents/CharacterController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.PlayerComponents {
@@ -8,9 +9,11 @@ namespace Game.PlayerComponents {
 
 		public Vector3 Gravity = Physics.gravity;
 		public float GravityMultiplier = 1.0f;
-		[Range(0, 0.5f)] public float SkinWidth = 0.15f;
+		[Range(0, 0.5f)] public float VerticalSkinWidth = 0.15f;
+		[Range(0, 0.5f)] public float HorizontalSkinWidth = 0.15f;
 		[Range(0, 1.0f)] public float SlopeInclinationAllowance = 0.1f;
 		[Range(0, 1.0f)] public float StepAllowance = 0.0f;
+		public float GrounderDistance = 2.0f;
 
 		private CapsuleCollider _collider;
 		private int _layerMaskAllButPlayer;
@@ -43,8 +46,10 @@ namespace Game.PlayerComponents {
 
 		public void Update() {
 			Debug.DrawRay(transform.position, _charMovement.WorldDir, Color.cyan);
+			Vector3 gravityForce = Vector3.zero;
 			if (!IsGrounded) {
-				_charMovement.AddForce(Gravity, _timeOnAir * Time.deltaTime);
+				gravityForce = Gravity * _timeOnAir * Time.deltaTime;
+				_timeOnAir += Time.deltaTime;
 			}
 			Matrix4x4 M = _collider.transform.localToWorldMatrix;
 			Vector3 capsuleHead = transform.position + M.MultiplyVector(_collider.center) +
@@ -52,17 +57,19 @@ namespace Game.PlayerComponents {
 			Vector3 capsuleFeet = transform.position + M.MultiplyVector(_collider.center) -
 			                      transform.up * (_collider.height / 2.0f - _collider.radius);
 
-
+			
 			CheckWalls(capsuleHead, capsuleFeet, transform.forward, PlayerAction.MoveForward); // TODO would be nice to have this independant of Action
 			CheckWalls(capsuleHead, capsuleFeet, -transform.forward, PlayerAction.MoveBack);
 			CheckWalls(capsuleHead, capsuleFeet, transform.right, PlayerAction.MoveRight);
 			CheckWalls(capsuleHead, capsuleFeet, -transform.right, PlayerAction.MoveLeft);
-
+			
 
 			//CheckFloor
-			RaycastHit[] floorHits = Physics.SphereCastAll(capsuleFeet, _collider.radius, -Vector3.up, SkinWidth,
+			RaycastHit[] floorHits = Physics.SphereCastAll(capsuleFeet, _collider.radius, -Vector3.up, GrounderDistance,
+				//VerticalSkinWidth,
 				_layerMaskAllButPlayer);
 			int collidersFound = 0;
+			Array.Sort(floorHits, (x, y) => x.distance.CompareTo(y.distance));
 			foreach (RaycastHit hit in floorHits) {
 				if (hit.collider.isTrigger) {
 					//hit.collider.SendMessage("OnTriggerEnter", _collider, SendMessageOptions.DontRequireReceiver);
@@ -71,13 +78,23 @@ namespace Game.PlayerComponents {
 				++collidersFound;
 				// overlapping collision
 				if (hit.point == Vector3.zero) {
-					_charMovement.AddForce(hit.normal, _charMovement.StepMovement.magnitude + SkinWidth / 2.0f);
+					//_charMovement.AddForce(hit.normal, _charMovement.StepMovement.magnitude + VerticalSkinWidth / 2.0f); // hit.normal is -ray.direction in this case
+					transform.position = transform.position + -gravityForce + Vector3.up * VerticalSkinWidth / 2.0f;
+						// * RecolocationSpeed;
 					IsGrounded = true;
+					break;
 				}
-				else if (hit.distance > SkinWidth / 2.0f) {
-					Debug.DrawRay(transform.position, hit.point - transform.position, Color.magenta);
-					_charMovement.AddForce(Vector3.down, hit.distance - SkinWidth/2.0f);
+				else if (hit.distance < VerticalSkinWidth) {
 					IsGrounded = true;
+					break;
+				}
+				else {
+					Debug.DrawRay(transform.position, hit.point - transform.position, Color.magenta);
+					//_charMovement.AddForce(Vector3.down, hit.distance - VerticalSkinWidth/2.0f);
+					//transform.position += Vector3.down * (hit.distance - VerticalSkinWidth / 2.0f);
+					transform.position += Vector3.down * (hit.distance - VerticalSkinWidth/2.0f);
+					IsGrounded = true;
+					break;
 				}
 			}
 
@@ -85,16 +102,20 @@ namespace Game.PlayerComponents {
 				IsGrounded = false;
 			}
 
-			if (!IsGrounded) {
-				_timeOnAir += Time.deltaTime;
+			if (!_isGrounded) {
+				transform.position += gravityForce;
 			}
+			else {
+				_timeOnAir = 0.0f;
+			}
+
 		}
 
 		private void CheckWalls(Vector3 capsuleHead, Vector3 capsuleFeet, Vector3 dir, PlayerAction playerActionType) {
 			int collidersFound = 0;
 			Vector3 stepOffset = transform.up * StepAllowance;
 			RaycastHit[] wallHits = Physics.CapsuleCastAll(capsuleHead, capsuleFeet + stepOffset, _collider.radius, dir,
-				4 * SkinWidth, _layerMaskAllButPlayer);
+				4 * HorizontalSkinWidth, _layerMaskAllButPlayer);
 			foreach (RaycastHit hit in wallHits) {
 				if (hit.collider.isTrigger) {
 					//hit.collider.SendMessage("OnTriggerEnter", _collider, SendMessageOptions.DontRequireReceiver);
@@ -102,14 +123,14 @@ namespace Game.PlayerComponents {
 				}
 				++collidersFound;
 				if (hit.point == Vector3.zero) {
-					_charMovement.AddForce(hit.normal, _charMovement.StepMovement.magnitude + SkinWidth / 2.0f);
+					_charMovement.AddForce(hit.normal, _charMovement.StepMovement.magnitude + HorizontalSkinWidth / 2.0f);
 				}
 				else if (Mathf.Abs(Vector3.Dot(Vector3.up, hit.normal)) >= (1 - SlopeInclinationAllowance)) {
 					continue;
 				}
-				else if (hit.distance > SkinWidth / 2.0f && hit.distance < SkinWidth) {
+				else if (hit.distance > HorizontalSkinWidth / 2.0f && hit.distance < HorizontalSkinWidth) {
 					Debug.DrawRay(transform.position, hit.point - transform.position, Color.magenta);
-					_charMovement.AddForce(-dir, (1 - Mathf.Abs(Vector3.Dot(Vector3.up, hit.normal))) * ((hit.distance - SkinWidth / 2.0f)));
+					_charMovement.AddForce(-dir, (1 - Mathf.Abs(Vector3.Dot(Vector3.up, hit.normal))) * ((hit.distance - HorizontalSkinWidth / 2.0f)));
 					var action = _actions.GetAction(playerActionType);
 					action.Disable();
 				}
