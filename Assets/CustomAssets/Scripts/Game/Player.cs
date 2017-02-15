07c11a91cca48f54b6ee51cae11d3def04f31a6f
Llikdowk <guillermofm7@gmail.com>
2017-02-15 19:06:58 +0100

    extract player states

--- Assets/CustomAssets/Scripts/Game/Player.cs
+++ Assets/CustomAssets/Scripts/Game/Player.cs
@@ -10,52 +10,6 @@ namespace Game.PlayerComponents {
 		Use, MoveForward, MoveLeft, MoveRight, MoveBack, Run
 	}
 
-	public abstract class PlayerState {
-
-		protected PlayerState() {
-			_movement = Player.GetInstance().Movement;
-			_transform = Player.GetInstance().transform;
-			_config = Player.GetInstance().Config;
-		}
-
-		protected CharacterMovement _movement;
-		protected Transform _transform;
-		protected SuperConfig _config;
-	}
-
-	public class WalkRunState : PlayerState {
-		public WalkRunState() {
-			_movement.MovementBehaviour = new WalkRunMovementBehaviour(_transform, _config);
-			_movement.MovementBehaviour.AddInteraction(new PickUpCoffinInteraction());
-			_movement.MovementBehaviour.AddInteraction(new CarveTerrainInteraction());
-		}
-	}
-
-	public class DragCoffinState : PlayerState {
-		public GameObject Coffin { get; private set; }
-		public DragCoffinState(GameObject coffin) {
-			Coffin = coffin;
-			_movement.MovementBehaviour = new DragCoffinBehaviour(_transform, coffin, _config);
-			_movement.MovementBehaviour.AddInteraction(new ThrowCoffinInteraction(coffin));
-			//_movement.MovementBehaviour.AvailableInteractions.Add(new SendCoffinToTombInteraction(coffin));
-		}
-	}
-
-	public class DriveCartState : PlayerState {
-		public DriveCartState(GameObject cart) {
-			_movement.MovementBehaviour = new CartMovementBehaviour(_transform, cart, _config);
-			_movement.MovementBehaviour.AddInteraction(new StopDrivingCartInteraction());
-		}
-	}
-
-
-	public class DigDownState : PlayerState {
-		public DigDownState(GameObject ground) {
-			_movement.MovementBehaviour = new DigBehaviour(_transform, ground);
-		}
-	}
-
-
 	[RequireComponent(typeof(CharacterController))]
 	public class Player : MonoBehaviour {
 
---
+++ Assets/CustomAssets/Scripts/Game/PlayerStates.cs
@@ -0,0 +1,52 @@
+﻿
+using Game.PlayerComponents;
+using Game.PlayerComponents.Movement;
+using Game.PlayerComponents.Movement.Behaviours;
+using Game.PlayerComponents.Movement.Behaviours.Interactions;
+using UnityEngine;
+
+namespace Game {
+	public abstract class PlayerState {
+		protected PlayerState() {
+			_movement = Player.GetInstance().Movement;
+			_transform = Player.GetInstance().transform;
+			_config = Player.GetInstance().Config;
+		}
+
+		protected CharacterMovement _movement;
+		protected Transform _transform;
+		protected SuperConfig _config;
+	}
+
+	public class WalkRunState : PlayerState {
+		public WalkRunState() {
+			_movement.MovementBehaviour = new WalkRunMovementBehaviour(_transform, _config);
+			_movement.MovementBehaviour.AddInteraction(new PickUpCoffinInteraction());
+			_movement.MovementBehaviour.AddInteraction(new CarveTerrainInteraction());
+		}
+	}
+
+	public class DragCoffinState : PlayerState {
+		public GameObject Coffin { get; private set; }
+		public DragCoffinState(GameObject coffin) {
+			Coffin = coffin;
+			_movement.MovementBehaviour = new DragCoffinBehaviour(_transform, coffin, _config);
+			_movement.MovementBehaviour.AddInteraction(new ThrowCoffinInteraction(coffin));
+			//_movement.MovementBehaviour.AvailableInteractions.Add(new SendCoffinToTombInteraction(coffin));
+		}
+	}
+
+	public class DriveCartState : PlayerState {
+		public DriveCartState(GameObject cart) {
+			_movement.MovementBehaviour = new CartMovementBehaviour(_transform, cart, _config);
+			_movement.MovementBehaviour.AddInteraction(new StopDrivingCartInteraction());
+		}
+	}
+
+
+	public class DigDownState : PlayerState {
+		public DigDownState(GameObject ground) {
+			_movement.MovementBehaviour = new DigBehaviour(_transform, ground);
+		}
+	}
+}
