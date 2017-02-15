30a412e9c8b7e45eb7f6758ab2f33c7d6f3b5280
Llikdowk <guillermofm7@gmail.com>
2017-02-15 21:28:29 +0100

    add Json.NET and perform a simple test with a poem mock up

---
+++ Assets/CustomAssets/Scripts/Game/Poems/PoemsPopulator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Game.Poems {
+
+	public struct LandmarkPoem {
+		public string[] Masculine;
+		public string[] Feminine;
+		public string[] Plural;
+		public string[] FirstPerson;
+	}
+
+	public class PoemsPopulator : MonoBehaviour {
+
+		public void Start() {
+			string jsonText = System.IO.File.ReadAllText("Assets/Resources/poems.json");
+			LandmarkPoem test = JsonConvert.DeserializeObject<LandmarkPoem>(jsonText);
+		}
+
+	}
+}
