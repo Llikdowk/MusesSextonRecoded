93bf1d105299c887ff8a9999f01567c94aa2ac3e
Llikdowk <guillermofm7@gmail.com>
2017-02-16 18:08:34 +0100

    add verses populator

--- Assets/CustomAssets/Scripts/Game/Poems/VersesPopulatorComponent.cs
+++ Assets/CustomAssets/Scripts/Game/Poems/VersesPopulatorComponent.cs
@@ -16,11 +16,17 @@ namespace Game.Poems {
 		//     vulture
 		//     well
 
-		
-
-		public void Start() {
+		public void Awake() {
 			string jsonText = System.IO.File.ReadAllText("Assets/Resources/poems.json");
-			LandmarkVerses[] test = JsonConvert.DeserializeObject<LandmarkVerses[]>(jsonText);
+			LandmarkVerses[] verses = JsonConvert.DeserializeObject<LandmarkVerses[]>(jsonText);
+			foreach (Transform child in gameObject.GetComponentsInChildren<Transform>()) {
+				foreach (LandmarkVerses v in verses) {
+					if (child.name.Contains(v.LandmarkName)) {
+						child.gameObject.AddComponent<LandmarkVersesComponent>().LandmarkVerses = v;
+						break;
+					}
+				}
+			}
 		}
 
 	}
