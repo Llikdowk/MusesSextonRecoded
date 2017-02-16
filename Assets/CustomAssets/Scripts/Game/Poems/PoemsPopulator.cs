866cbbd6f63c6771735634f407e8c79508d136bc
Llikdowk <guillermofm7@gmail.com>
2017-02-16 12:30:14 +0100

    rename some landmark references

--- Assets/CustomAssets/Scripts/Game/Poems/PoemsPopulator.cs
+++ Assets/CustomAssets/Scripts/Game/Poems/PoemsPopulator.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 namespace Game.Poems {
 
 
-	public struct LandmarkPoem {
-		public string LandmarkName;
-		public string[] Masculine;
-		public string[] Feminine;
-		public string[] Plural;
-		public string[] FirstPerson;
-	}
-
 	public class PoemsPopulator : MonoBehaviour {
+
+		public GameObject[] OtherLandmarks;
+		// dynamic:
+		//     cart
+		//     coffin
+		//     other grave
+		// 
+		// not 3d-modelled:
+		//     vulture
+		//     well
+
+		
+		private struct LandmarkPoem {
+			public string LandmarkName;
+			public string[] Masculine;
+			public string[] Feminine;
+			public string[] Plural;
+			public string[] FirstPerson;
+		}
 
 		public void Start() {
 			string jsonText = System.IO.File.ReadAllText("Assets/Resources/poems.json");
 			LandmarkPoem[] test = JsonConvert.DeserializeObject<LandmarkPoem[]>(jsonText);
-
 		}
 
 	}
